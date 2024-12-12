bb13fd5f61188b52066b62de84e77c76238647eb
Michael O'Brien <michael@obrienlabs.org>
2024-12-12 10:56:44 -0500

    #3 - start deployment.sh for k8s

--- src/kubernetes/eks/deployment.sh
+++ src/kubernetes/eks/deployment.sh
@@ -18,21 +18,15 @@ if [[ "$CREATE_PROJ" != false ]]; then
   STREAM_PROJECT_ID=${STREAM_PROJECT_NAME_PREFIX}-${STREAM_PROJECT_RAND}
   echo "Creating: $STREAM_PROJECT_ID"
 
+  createHostedZone
 
-  HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name $DOMAIN_NAME --query "HostedZones[0].Id" --output text)
-  echo $HOSTED_ZONE_ID
-  aws route53 get-hosted-zone --id ${HOSTED_ZONE_ID}
-  # check dns - delay needed
-  dig $DOMAIN_NAME ns +trace +nodnssec
-  # switch contexts without knowing the id
-  aws eks update-kubeconfig --region us-east-1 --name prod
-  # verify context star
-  kubectl config get-contexts
+  switchKubernetesContexts
 
+  deployCertManager
+  sleep 2
 
-  #kubectl apply -f clusterissuer-selfsigned.yaml
-  #envsubst < certificate.yaml | kubectl apply -f -
-
+  deployReloader
+  deployApps
 
 else
   #STREAM_PROJECT_ID=${STREAM_PROJECT_ID_PASSED}
@@ -41,9 +35,73 @@ fi
 
 if [[ "$DELETE_PROJ" != false ]]; then
   echo "Deleting: $STREAM_PROJECT_ID"
+  undeployApps
+  undeployReloader
+  undeployCertManager
+
 fi
 
 }
+
+createHostedZone() {
+    HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name --dns-name $DOMAIN_NAME --query "HostedZones[0].Id" --output text)
+  echo $HOSTED_ZONE_ID
+  aws route53 get-hosted-zone --id ${HOSTED_ZONE_ID}
+  # check dns - delay needed
+  dig $DOMAIN_NAME ns +trace +nodnssec
+}
+
+switchKubernetesContexts() {
+    # switch contexts without knowing the id
+  aws eks update-kubeconfig --region us-east-1 --name prod
+  # verify context star
+  kubectl config get-contexts
+}
+
+deployCertManager() {
+  echo "Deploy cert-manager"
+  helm install cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --namespace cert-manager \
+  --create-namespace \
+  --set crds.enabled=true
+
+  cmctl status certificate www
+  cmctl inspect secret www-tls
+
+  kubectl apply -f clusterissuer-selfsigned.yaml
+  envsubst < certificate.yaml | kubectl apply -f -
+}
+
+undeployCertManager() {
+  echo "unDeploy cert-manager"
+}
+
+deployReloader() {
+  echo "Deploy reloader"
+}
+
+undeployReloader() {
+  echo "unDeploy reloader"
+}
+
+deployApps() {
+  echo "Deploy apps"
+  kubectl apply -f deployment.yaml
+  sleep 5
+  kubectl apply -f service.yaml
+  # wait for 
+  # helloweb   LoadBalancer   10.100.90.190   af42541db9a3c4072b6f64a1852d17df-346026318.us-east-1.elb.amazonaws.com   443:30921/TCP   6s
+  sleep 5
+  kubectl get service helloweb
+}
+
+undeployApps() {
+  echo "unDeploy apps"
+  kubectl delete -f deployment.yaml
+  kubectl delete -f service.yaml
+}
+
 
 
 UNIQUE=old
@@ -84,5 +142,6 @@ if [[ -z $UNIQUE ]]; then
   exit 1
 fi
 
-deployment "$CREATE_PROJ" "$DELETE_PROJ" "$PROVISION_PROJ" "$BOOT_PROJECT_ID" "$STREAM_PROJECT_ID"
+#deployment "$CREATE_PROJ" "$DELETE_PROJ" "$PROVISION_PROJ" "$BOOT_PROJECT_ID" "$STREAM_PROJECT_ID"
+deployApps
 printf "**** Done ****\n"
